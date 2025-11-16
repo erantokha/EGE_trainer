c4cbb42a559004c7ae44d85e92a83913b522a2b7
erantokha <100702762+erantokha@users.noreply.github.com>
2025-11-17 03:47:40 +0400

    Delete tasks directory

--- tasks/shared/js/core/dom.js
+++
@@ -1,5 +0,0 @@
-
-export const $ = (s, root=document) => root.querySelector(s);
-export const $$ = (s, root=document) => Array.from(root.querySelectorAll(s));
-export const on = (el, ev, fn) => el && el.addEventListener(ev, fn);
-export const esc = (s) => String(s).replace(/[&<>"]/g, m => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;'}[m]));
--- tasks/shared/js/utils/csv.js
+++
@@ -1,6 +0,0 @@
-
-export function toCsv(rows){
-  const cols = Object.keys(rows[0] || {id:1});
-  const esc = v => '"' + String(v ?? '').replace(/"/g,'""') + '"';
-  return [ cols.join(','), ...rows.map(r => cols.map(c => esc(r[c])).join(',')) ].join('\n');
-}
