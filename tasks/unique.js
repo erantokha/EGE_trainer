b8ec6c96506362c8363f5afc99800f43e4f11e0f
erantokha <100702762+erantokha@users.noreply.github.com>
2026-01-14 12:17:18 +0000

    chore: bump build to 2026-01-14-1

--- tasks/unique.js
+++ tasks/unique.js
@@ -6,7 +6,7 @@
 const $ = (sel, root = document) => root.querySelector(sel);
 const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
 
-import { withBuild } from '../app/build.js?v=2026-01-06-1';
+import { withBuild } from '../app/build.js?v=2026-01-14-1';
 
 const INDEX_URL = '../content/tasks/index.json';
 
